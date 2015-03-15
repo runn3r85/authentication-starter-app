1f2f67fa8167b8d92927fe56acb86a6cfb294152
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-15 14:36:41 -0700

    add validations and downcase e-mail method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ActiveRecord::Base
 
-  validates :first_name, :last_name, :username, :email, presence: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: {
+                      with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                      message: 'Must be a valid e-mail format.'
+                    }
+
+  validates :username, presence: true,
+                      length: { minimum: 5, maximum: 30 },
+                      uniqueness: true,
+                      format: {
+                        with: /\A[a-zA-Z0-9]+\z/,
+                        message: 'Can only contain letters and numbers.'
+                      }, on: :create
+
+  validates :first_name, :last_name, presence: true
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
-    let(:user) { build(:user) }
+    let!(:user) { build(:user) }
 
     it "should require first_name" do
       expect(user).to validate_presence_of(:first_name)
@@ -14,15 +14,46 @@ RSpec.describe User, type: :model do
 
     it "should require username" do
       expect(user).to validate_presence_of(:username)
-      
+    end
+
+    it "should require unique username" do
+      expect(user).to validate_uniqueness_of(:username)
     end
 
     it "should require email" do
       expect(user).to validate_presence_of(:email)
     end
 
+    it "should require unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "TEST@example.com"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires the e-mail address to look like an email" do
+      user.email = "test"
+      expect(user).to_not be_valid
+    end
   end
 
+  describe "methods" do
+    let!(:user) { build(:user) }
 
+    context "#downcase_email" do
+      it "makes the email attribute lowercase" do
+        user.email = "TEST@EXAMPLE.COM"
+        user.downcase_email
+        expect(user.email).to eq("test@example.com") 
+      end
 
+      it "downcases an e-mail before saving" do
+        user.email = "TEST@EXAMPLE.COM"
+        expect(user.save).to be true
+        expect(user.email).to eq("test@example.com")
+      end
+    end
+  end
 end
